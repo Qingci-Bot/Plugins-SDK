af4ff9c2cfb8a0621a5d10d40c9f041d10d18e85
luoqingci <luoqingci@luoqingciya.com.cn>
2026-08-20 20:32:34 +0800

    release: v1.12.0（与 Qingci-Bot-CE 1.12.0 同步）

--- qingci_plugin_sdk/__init__.py
+++ qingci_plugin_sdk/__init__.py
@@ -7,7 +7,7 @@
     uv pip install -e .    # 从 Plugins-SDK 目录
 """
 
-__version__ = "1.11.0"
+__version__ = "1.12.0"
 
 from .base import PluginBase, PluginStatus
 from .context import MessageContext
